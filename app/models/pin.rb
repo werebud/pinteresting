b92aabf0748569faa364810e645ca7ea7c7f7bff
Adam Richard <werebud@gmail.com>
2015-08-10 19:45:46 -0700

    Added users and verifications, final stretch

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+	validates :description, presence: true
+	validates :image, presence: true
 end
